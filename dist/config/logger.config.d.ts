dbdf72dd411180d9a6c8e25962dec05e7a462170
@romuloisaias <romulo.hernandez@pinapp.tech>
2026-01-15 15:22:21 -0300

    depurando..

--- dist/config/logger.config.d.ts
+++
@@ -1,7 +0,0 @@
-import * as winston from 'winston';
-import 'winston-daily-rotate-file';
-export declare const winstonConfig: {
-    level: string;
-    format: winston.Logform.Format;
-    transports: (winston.transports.ConsoleTransportInstance | import("winston-daily-rotate-file"))[];
-};
